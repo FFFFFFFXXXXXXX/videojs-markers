01df0f40f1fb44598dc3a95e2ede2382c0e7ce91
FX <felix.arbeithuber@gmail.com>
2024-01-28 04:57:06 +0100

    progress

--- src/OrderedMap.ts
+++ src/OrderedMap.ts
@@ -1,14 +1,34 @@
 export default class OrderedMap<K, V> extends Map<K, V> {
 
     private readonly comparator;
+    private orderedValuesCached: ReadonlyArray<V> | null = null;
 
     public constructor(comparator: (v1: V, v2: V) => number) {
         super();
         this.comparator = comparator;
     }
 
-    public orderedValues(): Array<V> {
-        return Array.from(this.values()).sort(this.comparator)
+    public orderedValues(): ReadonlyArray<V> {
+        if (this.orderedValuesCached !== null) {
+            return this.orderedValuesCached;
+        } else {
+            return Array.from(this.values()).sort(this.comparator);
+        }
+    }
+
+    public override clear() {
+        this.orderedValuesCached = null;
+        super.clear();
+    }
+
+    public override delete(key: K): boolean {
+        this.orderedValuesCached = null;
+        return super.delete(key);
+    }
+
+    public override set(key: K, value: V): this {
+        this.orderedValuesCached = null;
+        return super.set(key, value);
     }
 
 }
